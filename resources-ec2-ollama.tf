ad379d7c41b6a70ad0f38045c632f4ed5074e974
Fabian Brash <fabianbrash@gmail.com>
2024-04-13 12:35:08 -0400

    Update resources-ec2-ollama.tf

--- resources-ec2-ollama.tf
+++ resources-ec2-ollama.tf
@@ -54,9 +54,9 @@ resource "aws_key_pair" "deployer" {
 
 resource "aws_instance" "example_ec2" {
   ami           = "ami-08116b9957a259459" # Replace with the actual AMI ID for Ubuntu 22.04 in your region
-  instance_type = "t2.2xlarge"
+  #instance_type = "t2.2xlarge"
   #instance_type = "g4ad.xlarge"  AMD GPU
-  #instance_type = "g3s.xlarge" NVIDIA GPU
+  instance_type = "g3s.xlarge" #NVIDIA GPU
   subnet_id     = aws_subnet.example_subnet.id
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
   #associate_public_ip_address = true   Request a public IP address
@@ -69,10 +69,11 @@ resource "aws_instance" "example_ec2" {
               sudo systemctl start ollama
               sudo systemctl enable ollama
               ollama pull llama2
+              ollama pull openchat
               EOF
   
    root_block_device {
-    volume_size           = 120
+    volume_size           = 200
     volume_type           = "gp2"
     delete_on_termination = true
   }
@@ -106,4 +107,35 @@ resource "aws_route_table" "example_rt" {
 resource "aws_route_table_association" "example_rta" {
   subnet_id      = aws_subnet.example_subnet.id
   route_table_id = aws_route_table.example_rt.id
+}
+
+
+output "instance_name" {
+  value = aws_instance.example_ec2.tags["Name"]
+  description = "The name of the EC2 instance"
+}
+
+output "instance_public_ip" {
+  value = aws_instance.example_ec2.public_ip
+  description = "The public IP address of the EC2 instance"
+}
+
+output "vpc_id" {
+  value = aws_subnet.example_subnet.vpc_id
+  description = "The VPC ID where the instance is deployed"
+}
+
+output "subnet_id" {
+  value = aws_instance.example_ec2.subnet_id
+  description = "The subnet ID where the instance is deployed"
+}
+
+output "vpc_arn" {
+  value = aws_vpc.example_vpc.arn
+  description = "Your VPC ARN"
+}
+
+output "ec2_instance_dns" {
+  value = aws_instance.example_ec2.public_dns
+  description = "Your instances public DNS"
 }
